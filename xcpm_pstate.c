ffa10eeb15f0078c2ad7e816218bd617af8f1f3d
V <vondturner@gmail.com>
2026-02-06 19:00:24 -0800

    Update xcpm_pstate.c

--- xcpm_pstate.c
+++ xcpm_pstate.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -21,49 +20,39 @@ typedef struct pstate_table {
     uint32_t high_frequency_modes_nonturbo[2];
     uint32_t max_efficent_frequency;
     uint32_t state_count;
-
+    
     pstate states[64];
-} pstate;
+} pstate_table;
 
 int main(int argc, const char * argv[]) {
     if (getuid() != 0) {
         printf("This tool requires root\n");
         return -1;
     }
-
-    int32_t fd;
-    pstate_table *table;
-    uint32_t pstatecnt;
-    uint64_t* ctr_a;
-    uint64_t* ctr_b;
     
-    double sumdiff = 0;
-    double freq = 0;
-        
-    // open xcpm
-    fd = open("/dev/xcpm", 0);
+    int32_t fd = open("/dev/xcpm", 0);
     if(fd == -1) perror("read");
     
-    // get pstate table
-    table = malloc(sizeof(pstate_table));
+    pstate_table *table = malloc(sizeof(pstate_table));
     ioctl(fd, XCPM_GET_PSTATE_TABLE, table, 0);
     
-    pstatecnt = table->state_count;
-
-    // sample ctrs
-    ctr_a = malloc(sizeof(uint64_t) * pstatecnt);
-    ctr_b = malloc(sizeof(uint64_t) * pstatecnt);
+    uint32_t pstatecnt = table->state_count;
+    
+    uint64_t* ctr_a = malloc(sizeof(uint64_t) * pstatecnt);
+    uint64_t* ctr_b = malloc(sizeof(uint64_t) * pstatecnt);
     
     ioctl(fd, XCPM_GET_PSTATE_CTRS, ctr_a, 0);
-    sleep(1); // update interval
+    sleep(1);
     ioctl(fd, XCPM_GET_PSTATE_CTRS, ctr_b, 0);
     
     printf("Max Efficient State: %u MHz\n", table->max_efficent_frequency);
     printf("Non-Turbo States: %u-%u MHz\n", table->high_frequency_modes_nonturbo[0], table->high_frequency_modes_nonturbo[1]);
     printf("Turbo States: %u-%u MHz\n\n", table->high_frequency_modes_turbo[0], table->high_frequency_modes_turbo[1]);
     printf("Requested Distribution: ");
-
-    // format to distribution
+    
+    double sumdiff = 0;
+    double freq = 0;
+    
     for (int i = pstatecnt; i--;) {
         sumdiff += ctr_b[i] - ctr_a[i];
     }
@@ -81,6 +70,6 @@ int main(int argc, const char * argv[]) {
     printf("Requested Package Frequency: %.2f MHz\n", freq);
     
     close(fd);
-
+    
     return 0;
 }
